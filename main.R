7954d1a1eb1a38449b913c5fb9701e7ddf192ffa
Benjamin Paul Suntrup <bpsxv3@mst.edu>
2017-11-02 12:51:09 -0500

    Flesh out more details

--- main.R
+++ main.R
@@ -3,19 +3,30 @@
 trainingData = read.csv("train.csv")
 novelData = read.csv("test.csv")
 
+# The following function will train my neural network with the testData
 trainer <- function(testData) {
 	weights = runif(n = 10, min = 0, max = 1)
 	return (weights)
 }
 
+# Giving this function a trained network (represented by weights) as well as
+# a datum to analyze, it returns a 1 or 0 representing Survived or Died 
+# respectively
 classifier <- function(datum, weights) {
 	return (1)
 }
 
 weights = trainer(trainingData)
 
+output <- data.frame(PassengerId = integer(), Survived = integer())
+
+# Construct the output data:
+
 for (x in 1:(nrow(novelData))) {
-	print(classifier(datum, weights))
+	output <- rbind(output, c(1,1))
 }
+
+
+print (output)
 
 
